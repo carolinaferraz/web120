6ab60f104c28c0397b8b48ec5999e9f649686741
cferraz <carolinapferraz@gmain.com>
2019-05-31 01:41:03 -0700

    updates webcam section w DOT
    livestream now has images not only sound (images from space).

--- big/webcam.php
+++ big/webcam.php
@@ -1,23 +1,27 @@
 <?php
 include ("includes/header-b.php");
 ?>
-
-
 <!-- START Wrapper -->
 <div class="wrapper">
 <!-- START LEFT COL -->
 <section>
 <h3>YouTube live stream example</h3>
-<div class='embed-container'><iframe src='https://www.youtube.com/embed/bebuiaSKtU4' frameborder='0' allowfullscreen></iframe></div>
-</div>
+<div class='embed-container'><iframe src='https://www.youtube.com/embed/4993sBLAzGA' frameborder='0' allowfullscreen></iframe></div>
+
 
 </section>
 <!-- END LEFT COL -->
 
 <!-- START RIGHT COL -->
 <aside>
+<h3>West Seattle DOT Camera</h3>
+<div class="dot">
+<iframe src="webcam.html"></iframe>
+
+</div>
 </aside>
 <!-- END RIGHT COL -->
+</div>
 </div>
 <?php
 include ("includes/footer.php");
