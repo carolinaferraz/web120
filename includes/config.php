1597564ed7de1492379a4b2a1b1069054023d171
carolinaferraz <43863678+carolinaferraz@users.noreply.github.com>
2019-08-07 03:08:57 -0700

    Delete config.php

--- includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-//include/config.php
-
-//Here are the keys for the server: 
-$siteKey = "6LduJ54UAAAAAFmk1nBZVxEzoY4gOwpPvJNathbo";
-$secretKey = "6LduJ54UAAAAAJ35s9d1Zb3yFucUE58ScKpUZaaD";
-
-?>
